06535c239fd4b6bacc1ba9f4672e38748adcb45d
Brandon <r.brandon.c@gmail.com>
2016-11-22 20:53:13 +0000

    Changed something

--- server.js
+++ server.js
@@ -4,8 +4,18 @@ var app = express();
 
 app.set('view engine', 'pug')
 
+app.use('/js', express.static(__dirname + '/js'));
 app.use('/css', express.static(__dirname + '/css'));
 app.use('/images', express.static(__dirname + '/images'));
+app.use('/github', express.static(__dirname + '/github'));
+
+app.post('/update', function(req, res) {
+	console.log("got a post");
+	var sys = require('sys')
+	var exec = require('child_process').exec;
+	function puts(error, stdout, stderr) { sys.puts(stdout) }
+	exec("sh github/update.sh", puts);
+});
 
 app.use('/', function(req, res) {
     res.render('index', {title: 'test'});
