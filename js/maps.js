9ae7918036fd1d7c6ab563cfc9978ca64216986e
Brandon <r.brandon.c@gmail.com>
2016-11-22 15:33:09 -0500

    Changed something

--- js/maps.js
+++ js/maps.js
@@ -1,24 +1,11 @@
 function initMap() {
-        var myLatLng = {lat: -25.363, lng: 131.044};
+  var myLatLng = {lat: -25.363, lng: 131.044};
 
         // Create a map object and specify the DOM element for display.
         var map = new google.maps.Map(document.getElementById('top'), {
           center: myLatLng,
           scrollwheel: false,
           zoom: 4
-
-          if(navigator.location){
-          navigator.geolocation.getCurrentPosition(function(position) {
-            var pos = {
-              lat: position.coords.latitude,
-              lng: position.coords.longitude
-            };
-
-            infoWindow.setPosition(pos);
-            infoWindow.setContent('Location found.');
-            map.setCenter(pos);
-          }
-        }
         });
 
         // Create a marker and set its position.
@@ -27,4 +14,17 @@ function initMap() {
           position: pos,
           title: 'Vacation Destination'
         });
+
+        if(navigator.location){
+            navigator.geolocation.getCurrentPosition(function(position) {
+              var pos = {
+                lat: position.coords.latitude,
+                lng: position.coords.longitude
+              };
+
+              infoWindow.setPosition(pos);
+              infoWindow.setContent('Location found.');
+              map.setCenter(pos);
+            }
+          }
       }
